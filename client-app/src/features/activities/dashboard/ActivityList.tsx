6c75aa37ff9367f1f7bc904513c6cfc058e61b45
harryazawa <victorlbamorim@gmail.com>
2023-05-28 18:24:57 -0300

    refactor: added use of mobx to project

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,49 +1,58 @@
-import React, { SyntheticEvent, useState } from 'react';
-import { Activity } from '../../../app/models/activity';
-import { Button, Item, Label, Segment } from 'semantic-ui-react';
+import React, { SyntheticEvent, useState } from "react";
+import { Button, Item, Label, Segment } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-interface Props{
-    activities: Activity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
+export default observer(function ActivityList() {
+  const { activityStore } = useStore();
 
-export default function ActivityList({activities, selectActivity, deleteActivity, submitting}: Props){
-    const [target, setTarget] = useState('');
+  const { deleteActivity, activitiesByDate, loading } = activityStore;
 
-    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string){
-        setTarget(e.currentTarget.name);
-        deleteActivity(id);
-    }
+  const [target, setTarget] = useState("");
 
-    return (
-        <Segment>
-            <Item.Group divided>
-                {activities.map(activity => (
-                    <Item key={activity.id}>
-                        <Item.Content>
-                            <Item.Header as='a'>{activity.title}</Item.Header>
-                            <Item.Meta>{activity.date}</Item.Meta>
-                            <Item.Description>
-                                <div>{activity.description}</div>
-                                <div>{activity.city}, {activity.venue}</div>
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='purple' />
-                                <Button
-                                    name={activity.id}
-                                    loading={submitting && target === activity.id}
-                                    onClick={(e) => handleActivityDelete(e, activity.id)}
-                                    floated='right'
-                                    content='Delete'
-                                    color='black' />
-                                <Label basic content={activity.category} />
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
-                ))}
-            </Item.Group>
-        </Segment>
-    )
-}
+  function handleActivityDelete(
+    e: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) {
+    setTarget(e.currentTarget.name);
+    deleteActivity(id);
+  }
+
+  return (
+    <Segment>
+      <Item.Group divided>
+        {activitiesByDate.map((activity) => (
+          <Item key={activity.id}>
+            <Item.Content>
+              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Meta>{activity.date}</Item.Meta>
+              <Item.Description>
+                <div>{activity.description}</div>
+                <div>
+                  {activity.city}, {activity.venue}
+                </div>
+              </Item.Description>
+              <Item.Extra>
+                <Button
+                  onClick={() => activityStore.selectActivity(activity.id)}
+                  floated="right"
+                  content="View"
+                  color="purple"
+                />
+                <Button
+                  name={activity.id}
+                  loading={loading && target === activity.id}
+                  onClick={(e) => handleActivityDelete(e, activity.id)}
+                  floated="right"
+                  content="Delete"
+                  color="black"
+                />
+                <Label basic content={activity.category} />
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        ))}
+      </Item.Group>
+    </Segment>
+  );
+});
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,33 +1,44 @@
-import React from 'react';
-import { Button, Card, Image } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
+import React from "react";
+import { Button, Card, Image } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
-interface Props{
-    activity: Activity
-    cancelSelectActivity: () => void;
-    openForm: (id: string) => void;
+export default function ActivityDetails() {
+  const { activityStore } = useStore();
+  const {
+    selectedActivity: activity,
+    openForm,
+    cancelSelectedActivity,
+  } = activityStore;
 
-}
+  if (!activity) return <LoadingComponent />;
 
-export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props){
-    return (
-        <Card fluid>
-            <Image src={`/assets/categoryImages/${activity.category},jpg`} />
-            <Card.Content>
-                <Card.Header>{activity.title}</Card.Header>
-                <Card.Meta>
-                    <span>{activity.date}</span>
-                </Card.Meta>
-                <Card.Description>
-                    {activity.description}
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <Button.Group width='2'>
-                    <Button onClick={() => openForm(activity.id)}basic color='purple' content='Edit' />
-                    <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel' />
-                </Button.Group>
-            </Card.Content>
-        </Card>
-    )
+  return (
+    <Card fluid>
+      <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+      <Card.Content>
+        <Card.Header>{activity.title}</Card.Header>
+        <Card.Meta>
+          <span>{activity.date}</span>
+        </Card.Meta>
+        <Card.Description>{activity.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group width="2">
+          <Button
+            onClick={() => openForm(activity.id)}
+            basic
+            color="purple"
+            content="Edit"
+          />
+          <Button
+            onClick={cancelSelectedActivity}
+            basic
+            color="grey"
+            content="Cancel"
+          />
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  );
 }
